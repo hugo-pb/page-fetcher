9adf2e1c501bf35da83e157689b2b2ac1de93f94
hugo-pb <h.palomerabecerra@gmail.com>
2022-08-31 22:13:09 -0600

    added request and print data

---
+++ fetcher.js
@@ -0,0 +1,9 @@
+const request = require("request");
+
+const link = process.argv[2];
+
+request(link, (error, response, body) => {
+  console.log("error:", error); // Print the error if one occurred
+  console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
+  console.log("body:", body); // Print the HTML for the Google homepage.
+});
