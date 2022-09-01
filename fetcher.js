bb3a7c46c3e69651a6ab4caa34620863d1935fd1
hugo-pb <h.palomerabecerra@gmail.com>
2022-08-31 22:35:39 -0600

    require fs

--- fetcher.js
+++ fetcher.js
@@ -1,9 +1,10 @@
 const request = require("request");
+const fs = require("fs");
 
 const link = process.argv[2];
-
+let data;
 request(link, (error, response, body) => {
   "error:", error;
   "statusCode:", response && response.statusCode;
-  console.log("body:", body);
+  data = body;
 });
